96340b30afa6d84e03c3957cf4b2ee42016dee8f
francisco santoyo <franciscosantoyo@Host-001.lan>
2025-05-13 10:58:26 +0200

    feat(candidates): add new candidates page

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { CandidatesComponent } from './pages/candidates/candidates.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: 'candidates', component: CandidatesComponent },
+    { path: '', redirectTo: '/candidates', pathMatch: 'full' },
+    { path: '**', redirectTo: '/candidates', pathMatch: 'full' },
+];
