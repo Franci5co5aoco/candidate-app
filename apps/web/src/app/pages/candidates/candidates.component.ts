d62784ddd76722082c05e71b662df55130a169cf
francisco santoyo <franciscosantoyo@Host-001.lan>
2025-05-13 13:03:44 +0200

    feat(forms): add functional form structure

--- apps/web/src/app/pages/candidates/candidates.component.ts
+++ apps/web/src/app/pages/candidates/candidates.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Candidate } from '@candidate-app/shared';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-candidates',
-  imports: [CommonModule,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
-    MatInputModule],
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+],
   templateUrl: './candidates.component.html',
-  styleUrl: './candidates.component.css',
+  styleUrl: './candidates.component.scss',
 })
 export class CandidatesComponent {
+  private fb = inject(FormBuilder)
+
   candidate: Candidate = {
     name: '',
     surname: '',
@@ -20,4 +32,23 @@ export class CandidatesComponent {
     yearsOfExperience: 0,
     availability: true
   };
+
+  
+
+  candidateForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+    surname: ['', [Validators.required,  Validators.minLength(3), Validators.maxLength(20)]],
+    seniority: ['', [Validators.required]],
+    yearsOfExperience: ['0', [Validators.required, Validators.min(0), Validators.max(70)]],
+    availability: [true]
+  });
+
+  onSubmit() {
+    if (this.candidateForm.valid) {
+      const formValues = this.candidateForm.value;
+      console.log('Form submitted:', formValues);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
