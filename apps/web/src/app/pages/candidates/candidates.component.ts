27b7e33738152762a0d61cdc8bed588ee67fcb57
francisco santoyo <franciscosantoyo@Host-001.lan>
2025-05-13 12:12:26 +0200

    feat(material): add Angular Material to project

--- apps/web/src/app/pages/candidates/candidates.component.ts
+++ apps/web/src/app/pages/candidates/candidates.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Candidate } from '@candidate-app/shared';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-candidates',
-  imports: [CommonModule],
+  imports: [CommonModule,
+    MatFormFieldModule,
+    MatInputModule],
   templateUrl: './candidates.component.html',
   styleUrl: './candidates.component.css',
 })
